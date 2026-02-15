1f9a81f8a6d98e2fe634a3c9728de907005de050
theextraordinary <shivangsharma074@gmail.com>
2026-02-15 04:05:36 +0000

    Added a print

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ except:
     SPARSE_ADAM_AVAILABLE = False
 
 def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoint_iterations, checkpoint, debug_from):
-
+    print("training")
     if not SPARSE_ADAM_AVAILABLE and opt.optimizer_type == "sparse_adam":
         sys.exit(f"Trying to use sparse adam but it is not installed, please install the correct rasterizer using pip install [3dgs_accel].")
 
